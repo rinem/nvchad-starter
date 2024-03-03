53855b381af429cb96edd19d673f56d1b410c04e
Roshan Mishra <roshanmishrarinem@gmail.com>
2024-03-03 19:34:16 +0530

    updated dashboard config

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,5 +1,43 @@
 local plugins = {
   {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper',
+        config = {
+          week_header = {
+           enable = true,
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Apps',
+              group = 'DiagnosticHint',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+        },
+      }
+    end,
+    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  },
+  {
 		"echasnovski/mini.animate",
 		event = "VeryLazy",
 		opts = function(_, opts)
@@ -23,7 +61,7 @@ local plugins = {
     config = function ()
         require'alpha'.setup(require'alpha.themes.dashboard'.config)
     end
-  };
+  },
   {
     "rcarriga/nvim-notify",
     event = "BufWinEnter",
